a8cd88df2b4a8f3b82cafb03c45b2e07713a4fb3
LethaboK <lethabokhedama@gmail.com>
2026-05-24 15:44:27 +0200

    Add https://shulker-web.onrender.com to CORS_ORIGINS

--- api/app/core/config.py
+++ api/app/core/config.py
@@ -32,6 +32,7 @@ class Settings(BaseSettings):
         "http://127.0.0.1:5173",
         "http://127.0.0.1:3000",
         "http://0.0.0.0:3000",
+        "https://shulker-web.onrender.com",
     ]
 
     @property
